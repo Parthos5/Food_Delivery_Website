4ab913ab8e14828c93f1329cb2aa7788bdb24a66
Parth Puranik <thosparth@gmail.com>
2023-06-03 00:12:24 +0530

    Basic setup and routes

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Login from "./screens/Login";
+import Home from "./screens/Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/screens/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../component/Navbar'
+import Footer from '../component/Footer'
+
+
+export default function Home() {
+  return (
+    <div>
+    <div>
+        <Navbar />
+        <div>Body</div>
+        <Footer />
+    </div>
+      
+    </div>
+  )
+}
